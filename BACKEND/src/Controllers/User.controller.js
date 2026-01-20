ed17ba03604ae2e2b7643d91c57473aa642e2b5b
Uttam Yadav <uttamyadav9560@gmail.com>
2026-01-21 02:45:56 +0530

    .

--- BACKEND/src/Controllers/User.controller.js
+++ BACKEND/src/Controllers/User.controller.js
@@ -142,7 +142,7 @@ const logout= AsyncHandler(async(req,res,next)=>{
             new:true
         }
     )
-    const isProduction = process.env.NODE_ENV === "production";
+    const isProduction = process.env.NODE_ENV === "production"; 
 
      const options={
         httpOnly:true,
--- FRONTEND/src/Services/Api.js
+++ FRONTEND/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_PRODUCTION==="production" ? import.meta.env.VITE_API_BASE_URL : import.meta.env.VITE_API_BASE_URL_local,
+  baseURL:  import.meta.env.VITE_PRODUCTION==="production" ? import.meta.env.VITE_API_BASE_URL : import.meta.env.VITE_API_BASE_URL_local, 
   withCredentials: true
 });
 
